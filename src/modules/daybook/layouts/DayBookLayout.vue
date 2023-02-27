2887f30118049f613d2905c7c9c3f2db55e4ee03
Alexander Mendivil <alexanderima@outlook.es>
2023-02-26 18:24:20 -0700

    navbar and layout

--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,3 +1,21 @@
 <template>
-    <h1>DayBook Layout</h1>
+    <NavBar />
+    <div class="col-4">
+    <EntryList/>        
+    </div>
+    <div class="col">
+        <router-view/>
+    </div>
 </template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default{
+    components:{
+        NavBar: defineAsyncComponent(() => import('../components/NavBar') ),
+        EntryList: defineAsyncComponent(() => import('../components/EntryList') )
+
+    }
+}
+</script>
