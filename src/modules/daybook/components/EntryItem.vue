2bc977fbb65ea7253b287860f4496372b9ed5fc2
Alexander Mendivil <alexanderima@outlook.es>
2023-03-04 18:35:53 -0700

    entry list, entry item

---
+++ src/modules/daybook/components/EntryItem.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="entry-container mb-3 pointer p2">
+
+        <div class="entry-title d-flex">
+            <span class="text-success fs-5 fw-bold">15 </span>
+            <span class="mx-1 fs-5">Julio </span>
+            <span class="mx-2 fw-light">2021</span>
+        </div>
+
+        <div class="entry-description">
+            Amet nisi mollit proident magna. In labore nulla consequat dolore reprehenderit. Aute incididunt proident tempor pariatur enim dolore minim elit ex excepteur. Culpa pariatur magna veniam duis sit dolor. Quis ipsum eiusmod nostrud nisi ipsum laborum quis consectetur. Elit deserunt adipisicing dolor dolore adipisicing dolor ullamco velit anim ipsum.
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-container{
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+    
+    &:hover{
+        background-color: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+    
+    .entry-description{
+        font-size: 12px;   
+    }
+}
+
+
+</style>
--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,13 +1,32 @@
 <template>
-  <h1>EntryList</h1>
+  <div class="entry-list-container">
+    <div class="px-2 pt-2">
+      <input type="text" class="form-control" placeholder="Buscar entrada">
+    </div>
+
+    <div class="entry-scrollarea">
+      <EntryItem v-for="item in 100" :key="item" />
+    </div>
+  </div>
 </template>
 
 <script>
+import { defineAsyncComponent } from 'vue'
 export default {
-
+  components:{
+    EntryItem: defineAsyncComponent(()=> import('./EntryItem.vue'))
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .entry-list-container{
+    border: 1px solid #2c3e50;
+    height: calc( 100vh - 56px );
+  }
 
+  .entry-scrollarea{
+    height: calc( 100vh - 120px );
+    overflow-y: scroll;
+  }
 </style>
