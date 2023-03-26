08bb07a007fbe715878adc6e39ecf15643d4c9c5
Alexander Mendivil <alexanderima@outlook.es>
2023-03-25 19:47:33 -0700

    entries

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,23 @@
 export default () =>({
     isLoading: true,
-    entries: []
+    entries: [
+        {
+        id: new Date().getTime(),
+        date: new Date().toDateString(),
+        text: 'In laboris incididunt do occaecat nisi incididunt id dolor irure. Incididunt eu quis aliqua cillum esse cupidatat irure velit proident commodo aliquip. Sunt ea commodo velit Lorem sint proident qui aliquip voluptate. Laborum voluptate nisi minim nisi ut voluptate Lorem qui voluptate cillum et anim. Minim enim fugiat pariatur reprehenderit occaecat officia cillum aliquip sint. Sit ea esse elit et irure dolore ea ex. Ullamco reprehenderit anim cillum officia sint consequat in ullamco ex culpa aliqua.',
+        picture: null
+    },
+    {
+        id: new Date().getTime()*Math.random(),
+        date: new Date().toDateString(),
+        text: 'Elit ex pariatur elit do. Fugiat ad excepteur elit non culpa officia cupidatat Lorem et sunt ad minim. Eiusmod proident enim sunt voluptate nisi consequat aliquip tempor officia aliqua excepteur incididunt ipsum. Fugiat anim qui culpa ex incididunt aliquip quis reprehenderit culpa enim adipisicing non.',
+        picture: null
+    },
+    {
+        id: new Date().getTime()*Math.random(),
+        date: new Date().toDateString(),
+        text: 'Qui consequat cillum duis reprehenderit. Deserunt dolore fugiat tempor aute dolor magna commodo aliqua magna velit velit proident. Esse occaecat mollit irure cillum laboris amet reprehenderit labore enim. Deserunt velit amet veniam consequat irure voluptate consequat aliqua veniam proident nulla quis aute labore. Id officia velit officia ipsum laboris esse adipisicing adipisicing veniam irure labore amet. Dolor dolor excepteur eiusmod sint nostrud ullamco ullamco nulla ea aliqua enim deserunt aute ullamco.',
+        picture: null
+    }
+]
 })
